c79aee7d14a5ab6d803e62c124712660642488d6
Sethi, Pinkesh <Pinkesh_Sethi2@intuit.com>
2017-09-08 00:28:09 +0530

    Adding initial app for Gyanodya.

---
+++ app/src/main/java/app/gyanodya/com/gyanodya/AboutUsActivity.java
@@ -0,0 +1,19 @@
+package app.gyanodya.com.gyanodya;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by psethi2 on 9/7/17.
+ */
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_us_activity);
+    }
+}
